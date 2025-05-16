491218b927b0a8ffb06a0102e08d0db38d154bda
thayoon <thagyun@naver.com>
2025-05-17 01:01:47 +0900

    config: init pjt, supabase

---
+++ src/app/search/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div>Search 페이지</div>;
+}
---
+++ src/utils/supabase/server.ts
@@ -0,0 +1,53 @@
+"use server";
+
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { cookies } from "next/headers";
+import { Database } from "@/../types_db";
+
+export const createServerSupabaseClient = async (
+  cookieStore: ReturnType<typeof cookies> = cookies(),
+  admin: boolean = false
+) => {
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    admin
+      ? process.env.NEXT_SUPABASE_SERVICE_ROLE!
+      : process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        async get(name: string) {
+          const cookies = await cookieStore;
+          return cookies.get(name)?.value;
+        },
+        async set(name: string, value: string, options: CookieOptions) {
+          try {
+            const cookies = await cookieStore;
+            cookies.set({ name, value, ...options });
+          } catch (error) {
+            console.error("Cookie set failed:", (error as any).message);
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+        async remove(name: string, options: CookieOptions) {
+          try {
+            const cookies = await cookieStore;
+            cookies.set({ name, value: "", ...options });
+          } catch (error) {
+            console.error("Cookie set failed:", (error as any).message);
+            // The `delete` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    }
+  );
+};
+
+export const createServerSupabaseAdminClient = async (
+  cookieStore: ReturnType<typeof cookies> = cookies()
+) => {
+  return createServerSupabaseClient(cookieStore, true);
+};
